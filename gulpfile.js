e6db4ab1bb4d2b28aba19170eb187730b6c0b102
sexfat <bryant6831@gmail.com>
2022-09-01 10:18:18 +0800

    執行順序

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const {
+    src,
+    dest,
+    series,
+    parallel,
+    watch
+} = require('gulp');
+
+
+function defaultTask(cb){
+    //任務
+  console.log('start');
+  cb();
+}
+
+exports.task = defaultTask;
+
+// 任務A
+function taskA(cb){
+    console.log('a task');
+    cb();
+};
+
+//任務B
+function taskB(cb){
+    console.log('b task');
+    cb();
+};
+
+//同時執行 A B任務
+exports.sync = parallel(taskA , taskB);
+
+//先執行 A 在執行B
+exports.async = series(taskA , taskB);
+
+
+
+
+
+
